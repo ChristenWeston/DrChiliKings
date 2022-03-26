2f4f2b5fe7a3b09e12999c4a67900ed65f041f62
Christen Weston <paleeserecycle@gmail.com>
2022-03-26 11:47:05 -0700

    Fixed engineerMachineID typo

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class EngineerMachine
   {
-    public int EngineerMachinetId { get; set; }
+    public int EngineerMachineId { get; set; }
     public int EngineerId { get; set; }
     public int MachineId { get; set; }
     public virtual Engineer engineer { get; set; }
